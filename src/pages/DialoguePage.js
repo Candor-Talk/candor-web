dbb467d2012b7698a4bd996a0bd87c0044bcb3bb
Anshay Ghosh <anshay.ghosh@mail.utoronto.ca>
2021-05-07 20:14:34 -0400

    Adding routing

---
+++ src/pages/DialoguePage.js
@@ -0,0 +1,44 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './../CandorClasses.css';
+import './../BootstrapCustomizations.css';
+import { Container, Col, Row } from 'react-bootstrap';
+import DialogueCarousel from './../components/PostCarousel';
+import DialogueInfoCard from './../components/DialogueInfoCard';
+import DownloadPrompt from './../components/DownloadPrompt';
+import { useSelector, useDispatch } from 'react-redux';
+import { getDialogue, getPosts } from './../actions';
+
+// Column layout constants
+const colXs = 12;
+const colMd = 4;
+
+function DialoguePage({ match }) {
+  const dialogueMetaData = useSelector(state => state.dialogueMetaData);
+  const postsList = useSelector(state => state.posts);
+  const dispatch = useDispatch();
+
+  const dialogueID = match.params.id;
+  if (dialogueID) {
+    dispatch(getDialogue(dialogueID));
+    dispatch(getPosts(dialogueID));
+  }
+  return (
+    <div className="Dialogue">
+      <Container fluid className="candor-container">
+        <Row className="candor-row" >
+          <Col xs={colXs} md={colMd} className="candor-col-height">
+           <DialogueInfoCard dialogueMetaData={dialogueMetaData}/>
+          </Col>
+          <Col xs={colXs} md={colMd} className="candor-col-height">
+            <DialogueCarousel postsList={postsList} />
+          </Col>
+          <Col xs={colXs} md={colMd} className="text-center d-flex align-items-end candor-col-height pb-5">
+            <DownloadPrompt />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default DialoguePage;

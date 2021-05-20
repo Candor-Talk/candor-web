aa0e443a735011a4c48b415a8351c341ebbcdaf4
Anshay Ghosh <anshay.ghosh@mail.utoronto.ca>
2021-05-19 22:23:33 -0400

    Added autoscroll for post completion and pausing

--- src/Components/VideoPlayer/PostPlayer.jsx
+++ src/Components/VideoPlayer/PostPlayer.jsx
@@ -5,7 +5,7 @@ import './post-player.css';
 const muxURLPrefix = "https://stream.mux.com/";
 const videoFileType = ".m3u8";
 
-const PostPlayer = ({post}) => {
+const PostPlayer = ({post, index, currentCarouselIndex, postDidCompletePlaying}) => {
   const url = muxURLPrefix + post.asset.playback_ids[0].id + videoFileType;
     return (
       <div className='player-wrapper'>
@@ -15,6 +15,9 @@ const PostPlayer = ({post}) => {
           width='100%'
           height='100%'
           controls={true}
+          loop={false}
+          playing={currentCarouselIndex===index}
+          onEnded={() => postDidCompletePlaying(index)}
         />
       </div>
     )
